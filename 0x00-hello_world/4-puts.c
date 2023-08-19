f762461d1aaa30b66e4707198d655acfbbd05386
Lena Mustafa <lenostf@gmail.com>
2023-08-19 11:54:50 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio. h>
+/**
+* main - Enter point
+*
+* Return  Always 0 (success )
+*\
+int main (void)
+{
+       puts("\programming is like building multilingual puzzle" );
+       return (0);
+}
